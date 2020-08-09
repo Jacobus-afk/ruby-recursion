9872cc289abd17b6a7329c03d617aa45a854d901
Jacobus-afk <s24162826@gmail.com>
2020-08-10 01:28:42 +0200

    tiny refactoring

--- lib/mergesort.rb
+++ lib/mergesort.rb
@@ -5,21 +5,21 @@ def merge(left_half, right_half)
   until left_half.empty? || right_half.empty?
     merge_arr << (left_half[0] > right_half[0] ? right_half.shift : left_half.shift)
   end
-  merge_arr.concat(left_half).concat(right_half)
+  merge_arr + left_half + right_half
 end
 
 def mergesort(arr)
   len = arr.length
-  half_len = len / 2
   return arr if len < 2
 
+  half_len = len / 2
   left_half = mergesort(arr[0..half_len - 1])
   right_half = mergesort(arr[half_len..-1])
 
   merge(left_half, right_half)
 end
 
-random_array = Array.new(21) { rand(1..20) }
+random_array = Array.new(21) { rand(1..200) }
 # random_array = [5, 6, 1, 3, 7, 8, 2, 4]
 puts "Unsorted: #{random_array}"
 sorted_arr = mergesort(random_array)
